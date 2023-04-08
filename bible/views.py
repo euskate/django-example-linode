fa7a4a9e131bce05bd469539f7e1d61d49f3f535
euskate <euskate07@gmail.com>
2023-04-08 22:00:56 +0900

    set bible figure

--- bible/views.py
+++ bible/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.core import serializers
 from django.views.generic import TemplateView
 from .models import BibleMetaModel, BibleModel, McheyneModel
+import datetime
 
 # Create your views here.
 
@@ -9,17 +11,20 @@ class BibleView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["meta"] = BibleMetaModel.objects.all()
+        context["meta_data"] = serializers.serialize('json', BibleMetaModel.objects.all())
         context["bible"] = BibleModel.objects.all()
+        context["bible_data"] = serializers.serialize('json', BibleModel.objects.all())
         return context
 
     template_name = 'bible/bible.html'
 
 
 class McheyneView(TemplateView):
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["mcheyne"] = McheyneModel.objects.all()
+        context["mcheyne_data"] = serializers.serialize('json', McheyneModel.objects.all())
+        context["bible_data"] = serializers.serialize('json', BibleModel.objects.all())
         return context
 
     template_name = 'bible/mcheyne.html'
